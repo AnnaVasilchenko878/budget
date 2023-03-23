4101a0c0ccb8076dfd23160f0d32bc36bc18e7f2
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-03-23 18:00:07 +0300

    Добавляет проверку ввода статьи расходов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let money = +prompt("Введите ваш бюджет на месяц: "), 
-    time = prompt("Введите дату в формате YYYY-MM-DD"),
-    optionalExpenses;
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
   budjet: money,
@@ -12,9 +11,13 @@ let appData = {
 };
 
 for(let i=0; i<2; i++) {
-  let a = +prompt("Ведите обязательную статью расходов в этом месяце:"),
+  let a = prompt("Введите обязательную статью расходов в этом месяце:"),
       b = +prompt("Во сколько это обойдется?");
-      appData.expenses[a] = b
+      if((typeof(a) === 'string' && typeof(a) != null && a != '' && a.length < 50) && (typeof(b) != null && b != '')) {
+        appData.expenses[a] = b;
+      } else {
+        // Дз написть цикл который будет спрашивать бюджет пока данные не будут введены правильно
+      }
 }
 
-alert(Math.round(appData.budjet/30),2);
+alert(appData.budjet/30);
