f47f315b129ac4d235d34f3e310888ed27fa7c77
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-03-24 16:07:53 +0300

    Добавляет цикл на верный ввод

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let money = +prompt("Введите ваш бюджет на месяц: "), 
     time = prompt("Введите дату в формате YYYY-MM-DD");
+  
 
 let appData = {
   budjet: money,
@@ -9,15 +10,29 @@ let appData = {
   income : [],
   savings: false
 };
+// запись данных 
+let a, b;
 
-for(let i=0; i<2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце:"),
-      b = +prompt("Во сколько это обойдется?");
-      if((typeof(a) === 'string' && typeof(a) != null && a != '' && a.length < 50) && (typeof(b) != null && b != '')) {
-        appData.expenses[a] = b;
-      } else {
-        // Дз написть цикл который будет спрашивать бюджет пока данные не будут введены правильно
-      }
+while(true) {
+  a = prompt("Введите обязательную статью расходов в этом месяце:"),
+  b = +prompt("Во сколько это обойдется?");
+  if((typeof(a) === 'string' && typeof(a) != null && a != '' && a.length < 50) && (typeof(b) != null && b != '')) {
+    appData.expenses[a] = b;
+    break;
+  }
 }
 
-alert(appData.budjet/30);
+//  бюджет на день
+appData.moneyPerDay = appData.budjet/30;
+alert("Ежеднеыный бюджет " + appData.moneyPerDay);
+
+// уровень достатка 
+if(appData.moneyPerDay<100) {
+  console.log('Минимальный уровень достатка')
+} else if(appData.moneyPerDay>100 && appData.moneyPerDay<2000) {
+  console.log('Средний уровень достатка')
+} else if(appData.moneyPerDay>2000) {
+  console.log('Высокий уровень достатка')
+} else {
+  console.log('Произошла ошибка')
+}
